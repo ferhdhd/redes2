1ac637cb0ae825d32e18c4bbb3794fbb91819c75
guiusa <guiusepeoneda@gmail.com>
2023-10-19 19:22:40 -0300

    the server returns information and can handle multiple requests at same time

--- client.py
+++ client.py
@@ -3,12 +3,11 @@
 import socket
 from configparser import ConfigParser
 
-
 def main():
 # Bloco de inicialização #######################################################
 	# Arquivo de configuração
 	cfg 		= ConfigParser()
-	cfg.read("./surpflix.ini")
+	cfg.read("./initFiles/godp.ini")
 	# Configurações de cliente
 	local_ip 	= cfg.get('client', 'ip')
 	local_port 	= int(cfg.get('client', 'port'))
@@ -17,13 +16,16 @@ def main():
 	server_port	= int(cfg.get('server', 'port'))
 	# Iniciando o socket
 	udp_socket 	= socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+	udp_socket.bind((local_ip, local_port))
 ################################################################################
 	
 	msg = input()
-	if msg == '':
-		msg = "Hello, World!"
-	
 	udp_socket.sendto(msg.encode(), (server_ip, server_port))
+
+	if msg == 'start':
+		ret = udp_socket.recv(1024).decode()
+		print(ret)
+
 	udp_socket.close()
 
 
--- server.py
+++ server.py
@@ -1,26 +1,37 @@
 #! /bin/python3
 
+# LIBS #########################################################################
 import socket
+import time
 from configparser import ConfigParser
+
+# Variáveis globais ############################################################
+init_return = "Iniciado Conexão!!"
 
 def main():
 # Bloco de inicialização #######################################################
 	# Arquivo de configuração
 	cfg			= ConfigParser()
-	cfg.read("./surpflix.ini")
+	cfg.read("./initFiles/godp.ini")
 	# Configurações de servidor
 	local_ip 	= cfg.get('server', 'ip')
 	local_port	= int(cfg.get('server', 'port'))
+	# Configurações de client
+	client_ip	= cfg.get('client', 'ip')
+	client_port	= int(cfg.get('client', 'port'))
 	# Iniciando o socket
 	udp_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM, 0)
 	udp_socket.bind((local_ip, local_port))
 ################################################################################
 
 	while (True):
-		data = udp_socket.recv(1024)
-		print(data.decode())
-		if (data.decode() == 'q'):
+		data = udp_socket.recv(1024).decode()
+		print(data)
+		if (data == 'start'):
+			udp_socket.sendto(init_return.encode(), (client_ip, client_port))
+		if(data == 'q'):
 			break
+		time.sleep(5)
 
 	udp_socket.close()
 
