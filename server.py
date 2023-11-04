522fc2c1aa060acc96dd81ab9ec3f11e7421e840
Fernando Gbur <fgs21@inf.ufpr.br>
2023-11-04 12:12:20 -0300

    server changing messaging time

--- server.py
+++ server.py
@@ -13,6 +13,7 @@ a = 1103515245
 b = 12345
 m = 1 << 31
 stop_threads = False
+msg_range = 0.5
 client_list = []
 
 # CLASSES ######################################################################
@@ -79,32 +80,35 @@ def main_send():
 			msg = (str(client.pkg)+","+roll)
 			client.pkg += 1
 			send(msg, address)
-		time.sleep(0.1)
+		time.sleep(msg_range)
 
-def main_interrupt():
+def main_inpt_handler():
+	print("Quando quiser mudar a frequência de envio das mensagens, pode digitar 'r 5', onde 5 é o intervalo desejado (em segundos), por exemplo")
+	print("O tempo padrão de envio das mensagens é 0.5")
+	
 	global stop_threads
+	global msg_range
 	udp_socket.settimeout(1)
 	while not stop_threads:
 		inpt = input()
 		if inpt == 'stop':
 			stop_threads = True
 			finish_clients()
-
-
-
-
+		elif inpt[0] == 'r':
+			msg_range = float(inpt.split(" ")[1])
+			print(msg_range)
 
 if __name__ == "__main__":
 	queue_thread 		= threading.Thread(target=main_queue)
 	send_thread 		= threading.Thread(target=main_send)
-	interrupt_thread	= threading.Thread(target=main_interrupt)
+	handler_thread	= threading.Thread(target=main_inpt_handler)
 
 	queue_thread.start()
 	send_thread.start()
-	interrupt_thread.start()
+	handler_thread.start()
 
 	queue_thread.join()
 	send_thread.join()
-	interrupt_thread.join()
+	handler_thread.join()
 
 	os.system("rm -rf __pycache__")
